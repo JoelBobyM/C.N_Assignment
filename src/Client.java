cd130fc131d0f44c166634c178925903b2e6a069
JoelBobyM <joelbobym@gmail.com>
2021-12-31 17:51:21 +0530

    Java-Program

--- src/Client.java
+++ src/Client.java
@@ -1,27 +1,97 @@
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
-import java.io.PrintWriter;
+
 class Client
 {
   Socket s;
   static Scanner sc = new Scanner(System.in);
+  static Scanner sci = new Scanner(System.in);
   PrintWriter pw;
   String in = "";
-  Client(String add , int port) throws Exception
+  ObjectInputStream obin;
+  BufferedReader reader;
+  String line;
+  Client(String add , int port)
   {
-    s = new Socket(add,port);
-    System.out.println("CONNECTED TO SERVER : " + add);
-    pw = new PrintWriter(s.getOutputStream(),true);
-    while(!in.equalsIgnoreCase("done"))
+    try
     {
-      in = sc.nextLine();
-      pw.println(in);
-      pw.flush();
+      s = new Socket(add,port);
+      System.out.println("SUCCESSFULLY CONNECTED TO SERVER");
+      pw = new PrintWriter(s.getOutputStream(),true);
+      obin = new ObjectInputStream(s.getInputStream());
     }
-    pw.close();
-    s.close();
+    catch (IOException e)
+    {
+      System.out.println("CONNECTION TO SERVER FAILED");
+    }
+    while(true)
+    {
+      System.out.println("\t\tMENU\n1.CREATE FILE IN SERVER'S DIRECTORY\n2.EDIT FILE IN SERVER'S DIRECTORY\n3.DISPLAY CONTENTS OF A FILE IN SERVER'S DIRECTORY\n4.DELETE A FILE IN SERVER'S DIRECTORY\n5.LIST ALL FILES IN SERVER'S DIRECTORY\n6.EXIT");
+      System.out.print("ENTER YOUR CHOICE : ");
+      int ch = sci.nextInt();
+      if(ch == 6)
+      {
+        pw.println("done");
+        pw.flush();
+        break;
+      }
+      else if(ch == 5)
+      {
+        pw.println("ls");
+        pw.flush();
+        try
+        {
+          reader = (BufferedReader) obin.readObject();
+          while ((line = reader.readLine()) != null)
+          {
+            System.out.println(line);
+          }
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+      }
+      else if(ch>=1 && ch<=4)
+      {
+        System.out.print("ENTER THE NAME OF FILE (WITH EXTENSION) : ");
+        in = sc.nextLine();
+        in = in.trim();
+        switch (ch)
+        {
+          case 1 ->
+          {
+            in = "touch " + in;
+            pw.println(in);
+            pw.flush();
+          }
+          case 3 ->
+          {
+            in = "cat " + in;
+            pw.println(in);
+            pw.flush();
+          }
+          case 4 ->
+          {
+            in = "rm " + in;
+            pw.println(in);
+            pw.flush();
+          }
+        }
+      }
+    }
+    try
+    {
+      pw.close();
+      s.close();
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO EXCEPTION "+e);
+    }
   }
-  public static void main(String[] args) throws Exception
+  public static void main(String[] args)
   {
     System.out.print("ENTER THE SERVER ADDRESS : ");
     String str = sc.nextLine();
--- src/Server.java
+++ src/Server.java
@@ -1,22 +1,42 @@
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.io.ObjectOutputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.ServerSocket;
 class Server
 {
   Socket s;
   ServerSocket ss;
-  BufferedReader in;
-  String o = "";
+  BufferedReader in,reader;
+  String o,c_Ip;
+  //String line = "";
+  Process process;
+  InetSocketAddress socketAddress;
+  ObjectOutputStream out;
   Server(int port) throws Exception
   {
     ss = new ServerSocket(port);
     s = ss.accept();
+    socketAddress = (InetSocketAddress) s.getRemoteSocketAddress();
+    c_Ip = socketAddress.getAddress().getHostAddress();
+    System.out.println("CONNECTED TO : "+ c_Ip);
     in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    while(!o.equalsIgnoreCase("done"))
+    out = new ObjectOutputStream(s.getOutputStream());
+    while(true)
     {
       o = in.readLine();
-      System.out.println(o);
+      if (o.equalsIgnoreCase("done"))
+      {
+        break;
+      }
+      process = Runtime.getRuntime().exec(o.trim());
+      reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      out.writeObject(reader);
+      /*while ((line = reader.readLine()) != null)
+      {
+        System.out.println(line);
+      }*/
     }
     s.close();
     in.close();
